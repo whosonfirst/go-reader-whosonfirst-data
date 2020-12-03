ff5b33985acfcdb79331cc3c4b266575dc4d906a
thisisaaronland <devnull@localhost>
2020-12-03 13:38:25 -0500

    start working on non-WOF WOF-like datasets

--- reader_test.go
+++ reader_test.go
@@ -2,10 +2,10 @@ package reader
 
 import (
 	"context"
-	"testing"
 	"github.com/whosonfirst/go-reader"
 	"io"
 	"io/ioutil"
+	"testing"
 )
 
 func TestWhosOnFirstDataReader(t *testing.T) {
@@ -33,5 +33,33 @@ func TestWhosOnFirstDataReader(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	
+
+}
+
+func TestSFOMuseumDataReader(t *testing.T) {
+
+	ctx := context.Background()
+
+	r, err := reader.NewReader(ctx, "whosonfirst-data://?organization=sfomuseum-data")
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rel_path := "172/956/253/7/1729562537.geojson"
+
+	fh, err := r.Read(ctx, rel_path)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer fh.Close()
+
+	_, err = io.Copy(ioutil.Discard, fh)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
 }
